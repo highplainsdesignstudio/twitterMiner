8c751f1ca9bbcacb764a6a1e394f7ea85a491155
Michael Hernandez <admin@highplainsdesignstudio.com>
2017-07-25 16:04:32 -0600

    Create env_variables.js
    This file must be modified to include personal Twitter application information.

---
+++ bin/env_variables.js
@@ -0,0 +1,8 @@
+/*
+ * Enter your personal Twitter application keys. 
+ */
+
+process.env.TWITTER_CONSUMER_KEY = '';
+process.env.TWITTER_CONSUMER_SECRET = '';
+process.env.TWITTER_ACCESS_TOKEN_KEY = '';
+process.env.TWITTER_ACCESS_TOKEN_SECRET = '';
