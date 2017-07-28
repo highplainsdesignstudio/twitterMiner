fd6548fc15d159a3e0f555d79df1098cb163eec7
Mike Hernandez <admin@highplainsdesignstudio.com>
2017-07-28 07:10:37 -0600

    v1.2.0: Added POST /embedTweets route with middleware to get oembedded status html from twitter.

--- routes/index.js
+++ routes/index.js
@@ -8,9 +8,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'HPDS Twitter Miner' });
 });
 
-router.get('/getTweets', twitterClient, function(req, res, next) {
-    console.log("Hello from /getTweets.");
-    res.send("Here is some data.");
+router.get('/getTweets', twitterClient.getTweets, function(req, res, next) {
+    console.log("You've gone too far!");
+    res.send("There was an error?...");
+});
+
+router.post('/embedTweets', twitterClient.getEmbeddedTweets, function (req, res, next) {
+    console.log("You've gone too far!");
+    res.send("There was an error?...");
 });
 
 module.exports = router;
