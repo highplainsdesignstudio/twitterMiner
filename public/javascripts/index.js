6d70ef5a77d2a8937e32dcd4b85ab19fa8c51730
Mike Hernandez <admin@highplainsdesignstudio.com>
2017-07-28 07:14:21 -0600

    v1.2.0: Added displayTweets() that makes a call to /embedTweets for every found tweet ID and loads them to the page.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,30 +1,33 @@
-var hashTags = [];
+// Global variables
+var hashTags = []; 
 var usersCloud = [];
 var wordCloud = [];
+var embeddedTweetsIDs = [];
 
 function getTweets() {
     // reset the clouds
     hashTags = [];
     usersCloud = [];
     wordCloud = [];
+    embeddedTweetsIDs = [];
+    $('#embedded-tweets').html('');
     
-    var term = $("#search-term").val();
+    // Obtain values from the inputs.
     var file = $("#search-name").val();
+    var term = $("#search-term").val();
     var latitude = $("#search-latitude").val();
     var longitude = $("#search-longitude").val();
     var distance = $("#search-distance").val();
     var resultType = $("input[name='resultType']:checked").val();
     
+    // Check that required inputs are filled out.
     if ((/\S/.test(term)) && (/\S/.test(file))) { 
-
-
         $('#search-name').val('');
         $('#search-term').val('');
-        
         $('#word-cloud').jQCloud('destroy');
 
 
-        // send the term to be searched for to the server.
+        // The parameters sent to the server.
         var params = {
             term: term,
             file: file,
@@ -37,11 +40,11 @@ function getTweets() {
         };
         
         $.get('/getTweets', params, function(data) {
-
             console.log(data);
-            $('#search-results').show().text("Found " + data.tweets.statuses.length + " statuses.\n" +
-                    "File Name: " + file + "\n" +
-                    "Query: " + term);
+            $('#search-results').show()
+                    .html("<p class='col-sm-4'>Found " + data.tweets.statuses.length + " statuses.</p>" +
+                    "<p class='col-sm-4'>File Name: " + file + "</p>" +
+                    "<p class='col-sm-4'>Query: " + term + '</p>');
 
             /*
              * Relevant variables for the word clouds. 
@@ -53,15 +56,18 @@ function getTweets() {
              * Cycle through statuses to extract the relevant variables. 
              */
             for (var i=0; i < data.tweets.statuses.length; i++) {
+                // extract hashtags
                 var tempHashtags = data.tweets.statuses[i].entities.hashtags;
                 hashtags.push(tempHashtags);
 
+                // extract user screen names
                 if (users.hasOwnProperty(data.tweets.statuses[i].user.screen_name)) {
                     users[data.tweets.statuses[i].user.screen_name]++;
                 } else {
                     users[data.tweets.statuses[i].user.screen_name] = 1;
                 }
-
+                
+                embeddedTweetsIDs.push(data.tweets.statuses[i].id_str);
             }
 
             /*
@@ -122,16 +128,24 @@ function getTweets() {
                 wordCloud.push(textObj);
             }
             
+            
+//            for (let i=0; i < embeddedTweetsIDs.length; i++) {
+//                var id = '#tweet-' + i;
+//                $('#embedded-tweets').append("<div id='"+id+"'></div>");
+//            }
+            
             /*
              * Set the initial word cloud to be the hashTags cloud.
              */
             $('#word-cloud').jQCloud(hashTags, {
                 height: 750,
                 autoResize: true,
-                colors: ['red', 'orange', 'yellow', 'green'],
+                colors: ['red', 'orange', 'green', 'black'],
                 fontSize: ['5em', '4em', '3em', '2em'],
                 steps: 4
             });
+            $('#get-tweets').show();
+            
         });
     } else { 
         console.log("is null"); 
@@ -151,5 +165,16 @@ function textCloud() {
     $('#word-cloud').jQCloud('update', wordCloud);
 }
 
+function displayTweets() {
+    console.log("DisplayTweets pressed.");
+    for (let i=0; i < embeddedTweetsIDs.length; i++) {
+        var id = 'tweet-' + i;
+        $('#embedded-tweets').append("<div id='"+id+"' class='col-sm-6'></div>");
+        $('#'+id).load('/embedTweets', {id: embeddedTweetsIDs[i]}, function() {
+            console.log("Stuff");
+            twttr.widgets.load();
+        });
+    }
+}
 
 
